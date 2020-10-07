7a77587457236c5607d55a854cc09155285cde96
asilvas <asilvas@godaddy.com>
2020-10-07 14:41:10 -0700

    brotli support

--- src/http/compress-file.js
+++ src/http/compress-file.js
@@ -2,21 +2,30 @@ const zlib = require('zlib');
 
 module.exports = ({ req, res, realContentType, headers, data }) => {
   const accept = req.headers['accept-encoding'] || '';
-  if (!/gzip/i.test(accept)) {
-    // all browsers support gzip, use it or nothing at all
-    return false;
-  }
-  const shouldGzip = !/^image|^binary/i.test(realContentType) && headers.Size >= 200; // gzipping below this size can make files bigger
-  if (!shouldGzip) return false;
+  const supportGzip = /\bgzip\b/.test(accept);
+  const supportBrotli = /\bbr\b/.test(accept);
+  const shouldCompress = 
+    (supportGzip || supportBrotli)
+    && !/^image|^binary/i.test(realContentType) && headers.Size >= 200 // gzipping below this size can make files bigger
+  ;
+  if (!shouldCompress) return false;
 
-  zlib.gzip(data, function (err, gzipBuffer) {
+  let compressor, contentEncoding;
+  if (supportBrotli) {
+    compressor = zlib.brotliCompress;
+    contentEncoding = 'br';
+  } else {
+    compressor = zlib.gzip;
+    contentEncoding = 'gzip';
+  }
+  compressor(data, function (err, compressBuffer) {
     if (err) {
       // if compression fails, log it and move on. no need to fail request
       return void res.end(data);
     }
 
-    res.setHeader('Content-Encoding', 'gzip');
-    res.end(gzipBuffer);
+    res.setHeader('Content-Encoding', contentEncoding);
+    res.end(compressBuffer);
   });
 
   return true;
